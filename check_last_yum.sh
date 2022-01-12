caa211f2f80a93e524174a20a54f36be4e821a6c
rich568117 <rich568117@gmail.com>
2022-01-12 12:15:29 -0800

    Changed logic syntax

--- check_last_yum.sh
+++ check_last_yum.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 yum history|grep update  > /tmp/lastupdate
 awk 'BEGIN{FS="|";OFS=","}{$1=$1; print}' /tmp/lastupdate > /tmp/lastupdate.csv
 awk -F "\"*,\"*" '{print $3}' /tmp/lastupdate.csv > /tmp/lastyum.log
@@ -8,14 +7,14 @@ datum2=`date "+%s"`
 diff=$(($datum2-$datum1))
 
 days=$(($diff/(60*60*24)))
-
+echo $days
 if [ $days == 0 ]; then
         echo "OK - Last update was run less than 30 days ago"
         exit 0
-        elif [ $days > 30 ]; then
+        elif [ $days -gt 30 ]; then
                 echo "CRITICAL - Over 30 days since last update!"
                 exit 2
-        elif [ $days < 30 ]; then
+        elif [ $days -lt 30 ]; then
                 echo "OK - Last update was run less than 30 days ago"
                 exit 0
         else
